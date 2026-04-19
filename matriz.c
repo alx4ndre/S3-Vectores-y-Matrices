3c106ee9403df036130ca4e4416fb3e45e3a3995
LuisOrozco2007 <luis.orozco.villacis@udla.edu.ec>
2026-04-19 11:55:08 -0500

    Validar que las notas ingresadas esten entre 0 y 10 - Luis Orozco

--- matriz.c
+++ matriz.c
@@ -9,12 +9,20 @@ int main() {
     float mayor, menor;
     int aprobados, reprobados;
 
+    // INGRESO DE NOTAS
     for (i = 0; i < 5; i++) {
         printf("\nEstudiante %d\n", i + 1);
 
         for (j = 0; j < 3; j++) {
-            printf("Ingrese la nota de la asignatura %d: ", j + 1);
-            scanf("%f", &notas[i][j]);
+            do {
+                printf("Ingrese la nota de la asignatura %d: ", j + 1);
+                scanf("%f", &notas[i][j]);
+
+                if (notas[i][j] < 0 || notas[i][j] > 10) {
+                    printf("Error: la nota debe estar entre 0 y 10\n");
+                }
+
+            } while (notas[i][j] < 0 || notas[i][j] > 10);
         }
     }
 
