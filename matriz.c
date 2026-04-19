699a8763984d007ff41432e9824a6c2955e0df6c
alx4ndre <mateo.mero@udla.edu.ec>
2026-04-18 23:18:24 -0500

    Agregar ingreso de notas por estudiante y asignatura

--- matriz.c
+++ matriz.c
@@ -9,5 +9,14 @@ int main() {
     float mayor, menor;
     int aprobados, reprobados;
 
+    for (i = 0; i < 5; i++) {
+        printf("\nEstudiante %d\n", i + 1);
+
+        for (j = 0; j < 3; j++) {
+            printf("Ingrese la nota de la asignatura %d: ", j + 1);
+            scanf("%f", &notas[i][j]);
+        }
+    }
+
     return 0;
 }
