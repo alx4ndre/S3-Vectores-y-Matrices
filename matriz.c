71083dcfaf5469cf1fb527f276ddf4c7bf295332
LuisOrozco2007 <luis.orozco.villacis@udla.edu.ec>
2026-04-18 21:34:03 -0500

    Crear estructura base del programa - Luis Orozco

---
+++ matriz.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+    float notas[5][3];
+    int i, j;
+
+    float suma, promedio;
+    float mayor, menor;
+    int aprobados, reprobados;
+
+    return 0;
+}
